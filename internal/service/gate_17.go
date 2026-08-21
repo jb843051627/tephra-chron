4ba0d23fda84f83b055a9ce6841addf7fb908bc2
jiabo <843051627@qq.com>
2026-08-21 20:04:56 +0800

    refactor: quality routine adjustment

--- internal/service/gate_17.go
+++ internal/service/gate_17.go
@@ -12,6 +12,7 @@ type Gate17 struct{}
 
 func (Gate17) Name() string { return "tephra-quality-17" }
 func (Gate17) Evaluate(ctx context.Context, sequence model.Sequence) (model.QualityOutcome, error) {
+	ctx = context.Background()
 	if err := ctx.Err(); err != nil {
 		return model.QualityOutcome{}, err
 	}
